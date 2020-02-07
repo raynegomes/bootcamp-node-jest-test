57c747324ee64916e36762dab0ebe32fffa79ffe
raynegomes <raynegomes@gmail.com>
2020-02-07 18:31:49 -0300

    Initial project commit

---
+++ README.md
@@ -0,0 +1,51 @@
+<center>
+  <h1>TDD in NODE JS with JEST</h1>
+</center>
+
+Project to test the implementation of TDD in NodeJs with Jest
+
+### How to implement
+
+- Clone or download the repository:
+
+```
+1. git clone https://github.com/raynegomes/bootcamp-node-jest-test.git
+2. cd server
+```
+
+- Install the dependences to the project:
+
+```
+1. yarn install
+```
+
+or
+
+```
+2. npm install
+```
+
+- Run the tests:
+
+```
+3. yarn test
+```
+
+or
+
+```
+3. npm test
+```
+
+### Packages used in the project:
+
+- Node
+- Express
+- Sucrase + Nodemon
+- Sequelize
+- Jest
+- Supertest
+- Sqlite3
+- Bcryptjs
+- Factory-girl
+- Faker
---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import bcrypt from 'bcryptjs';
+import app from '../../src/app';
+
+import factory from '../factories';
+import truncate from '../util/truncate';
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const pass = '123456';
+    const user = await factory.create('User', {
+      password: pass,
+    });
+
+    const compareHash = await bcrypt.compare(pass, user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
+  it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.status).toBe(400);
+  });
+});
